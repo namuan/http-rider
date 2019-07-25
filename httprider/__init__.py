adeca05dc5d1381ae9b5b8941f4568c40a5b9dd8
namuan <>
2019-07-25 21:19:17 +0100

    Manual imports to work around py2app limitations

--- httprider/__init__.py
+++ httprider/__init__.py
@@ -2,3 +2,7 @@ __version__ = '0.0.2'
 __appname__ = 'httprider'
 __description__ = 'Simple and Powerful cross-platform Rest API client'
 __desktopid__ = 'dev.deskriders.HttpRider'
+
+from pygments.styles.default import DefaultStyle
+import sqlalchemy.dialects.sqlite
+import sqlalchemy.sql.default_comparator
--- httprider/core/faker_config.py
+++ httprider/core/faker_config.py
@@ -1,8 +1,47 @@
-from faker import Faker
-from faker.providers import internet, person, misc, address
+from faker import Generator
+from faker.providers.address.en_US import Provider as AP
+from faker.providers.company.en_US import Provider as CP
+from faker.providers.internet.en_US import Provider as IP
+from faker.providers.misc.en_US import Provider as MP
+from faker.providers.person.en_US import Provider as PP
 
-fake = Faker()
-fake.add_provider(internet)
-fake.add_provider(person)
-fake.add_provider(misc)
-fake.add_provider(address)
+g = Generator()
+g.add_provider(PP)
+g.add_provider(MP)
+g.add_provider(IP)
+g.add_provider(AP)
+g.add_provider(CP)
+
+
+class CustomFaker:
+    last_name = g.last_name
+    first_name = g.first_name
+
+    country = g.country
+    building_number = g.building_number
+    street_name = g.street_name
+    street_address = g.street_address
+    address = g.address
+    secondary_address = g.secondary_address
+    city = g.city
+    zipcode = g.postcode
+    state = g.state
+
+    prefix_male = g.prefix_male
+    first_name_male = g.first_name_male
+    last_name_male = g.last_name_male
+    name_male = g.name_male
+    suffix_male = g.suffix_male
+
+    prefix_female = g.prefix_female
+    first_name_female = g.first_name_female
+    last_name_female = g.last_name_female
+    name_female = g.name_female
+    suffix_female = g.suffix_female
+    uuid4 = g.uuid4
+
+    def domain_word(self):
+        return g.domain_word()
+
+
+fake = CustomFaker()
