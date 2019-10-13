ff2cb7e5eed1c20b6c6f2e3bbfe1637747458323
namuan <575441+namuan@users.noreply.github.com>
2019-10-13 12:56:45 +0100

    [Improv] Styling QTabWidget and reduced surrounding spaces

--- httprider/debug_app.py
+++ httprider/debug_app.py
@@ -12,32 +12,15 @@ class DebugWindow(QtWidgets.QMainWindow, Ui_DebugWindow):
     def __init__(self, parent=None):
         super(DebugWindow, self).__init__(parent)
         self.setupUi(self)
+        self.tabWidget.setStyleSheet("""
+            border-top: 2px solid #C2C7CB;
+            left: 15px; /* move to the right by 5px */
 
+        """)
         # ui events
-        self.btn_test.clicked.connect(self.on_test)
-        self.btn_select.clicked.connect(self.on_set_select)
-        self.startVal.valueChanged.connect(self.on_update_select)
-        self.endVal.valueChanged.connect(self.on_update_select)
 
     def on_test(self):
-        whole_text = self.txt_val.text()
-        selected_text = self.txt_val.selectedText()
-        self.start = self.txt_val.selectionStart()
-        self.sel_length = self.txt_val.selectionLength()
-
-        output = f"W: {whole_text}, t: {selected_text}, s: {self.start}, l: {self.sel_length}"
-        self.lbl_output.setText(output)
-
-    def on_set_select(self):
-        self.txt_val.setSelection(
-            self.start,
-            self.sel_length
-        )
-
-    def on_update_select(self):
-        sv = self.startVal.value()
-        ev = self.endVal.value()
-        self.txt_val.setSelection(sv, ev)
+        pass
 
 
 if __name__ == '__main__':
