452881b27709729b90d2ff38ab591b9f99c68b0a
namuan <575441+namuan@users.noreply.github.com>
2022-02-06 06:58:28 +0000

    fix: Remove version pinning and upgrade deprecated mistune API

--- httprider/core/markdown_renderer.py
+++ httprider/core/markdown_renderer.py
@@ -4,7 +4,7 @@ from pygments.lexers import get_lexer_by_name
 from pygments.formatters import html
 
 
-class HighlightRenderer(mistune.Renderer):
+class HighlightRenderer(mistune.HTMLRenderer):
     def block_code(self, code, lang=None):
         if not lang:
             return "\n<pre><code>%s</code></pre>\n" % mistune.escape(code)
